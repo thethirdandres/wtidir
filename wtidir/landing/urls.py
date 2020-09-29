cc2d79654b823084d926d35eea07a9f148491ec1
Mae Dagum <mayeghen@gmail.com>
2020-09-29 14:41:07 +0800

    Created initial landing page

---
+++ wtidir/landing/urls.py
@@ -0,0 +1,8 @@
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.landing_view, name='landing_view'),
+]
---
+++ wtidir/landing/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def landing_view(request):
+    return render(request, 'landing_templates/landing.html')
