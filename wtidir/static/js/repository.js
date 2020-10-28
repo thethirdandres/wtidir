0f2b9fa465bcc0fc92a3189378bad28281d8d18b
Anders <thethirdandres@gmail.com>
2020-10-28 17:22:40 +0800

    Progress on Delivery

--- wtidir/static/js/repository.js
+++ wtidir/static/js/repository.js
@@ -11,11 +11,6 @@ $(document).ready(function() {
             });
         }
 
-        // var classList = document.getElementById('repo-edit-quota-button').className.split(/\s+/);
-        // for (var i = 0; i < classList.length; i++) {
-        //     console.log(classList[i]);
-        // }
-
         if ($(this).hasClass('in-branch')) {
             console.log(document.getElementById("repo-edit-quota-button").style.display);
             document.getElementById("repo-edit-quota-button").style.display === "block" ? document.getElementById("repo-edit-quota-button").style.display = "none" : document.getElementById("repo-edit-quota-button").style.display = "block";
---
+++ wtidir/static/js/sheets.js
@@ -0,0 +1,12 @@
+// $(document).ready(function() {
+//     $('.sheets-nav-link').click(function() {
+//         if ($(this).hasClass('in-branch')) {
+//             console.log(document.getElementById("repo-edit-quota-button").style.display);
+//             document.getElementById("repo-edit-quota-button").style.display === "block" ? document.getElementById("repo-edit-quota-button").style.display = "none" : document.getElementById("repo-edit-quota-button").style.display = "block";
+//             console.log("changed");
+//         }
+
+//     });
+
+
+// });
