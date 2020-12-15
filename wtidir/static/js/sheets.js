7208e13d6f5b6145b8949a8eb3e962c2bdfd54cd
Andres Tubelleja III <thethirdandres@gmail.com>
2020-12-15 16:52:38 +0800

    Delivery fix + added more modals

--- wtidir/static/js/sheets.js
+++ wtidir/static/js/sheets.js
@@ -6,19 +6,20 @@
 $(document).ready(function() {
     $('.delivery-table-documents').children('tbody').find('tr').click(function() {
         var showTable = $('.delivery-table-documents-content');
-        var row = showTable.insertRow(0);
-        var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
-        cell1.innerHTML = "NEW CELL1";
-        cell2.innerHTML = "NEW CELL2";
-        // showTable.children('tbody').find('tr').detach();
         // var row = showTable.insertRow(0);
-        // var itemCell = row.insertCell(0);
-        // var quantityCell = row.insertCell(1);
-        // var unitCell = row.insertCell(2);
+        // var cell1 = row.insertCell(0);
+        // var cell2 = row.insertCell(1);
+        // cell1.innerHTML = "NEW CELL1";
+        // cell2.innerHTML = "NEW CELL2";
 
-        // itemCell.innerHTML = "BBQ Sauce";
-        // quantityCell.innerHTML = "5000";
-        // unitCell.innerHTML = "bot";
+        // showTable.children('tbody').find('tr').detach();
+        var row = showTable.insertRow(0);
+        var itemCell = row.insertCell(0);
+        var quantityCell = row.insertCell(1);
+        var unitCell = row.insertCell(2);
+
+        itemCell.innerHTML = "BBQ Sauce";
+        quantityCell.innerHTML = "5000";
+        unitCell.innerHTML = "bot";
     });
 });
