92eb5afa654882f3fe464f348ef094f1344ffc98
Andres <thethirdandres@gmail.com>
2020-12-22 16:28:27 +0800

    Redesigns
    Add delivery button behavior
    unmapping

--- wtidir/static/js/sheets.js
+++ wtidir/static/js/sheets.js
@@ -6,20 +6,30 @@
 $(document).ready(function() {
     $('.delivery-table-documents').children('tbody').find('tr').click(function() {
         var showTable = $('.delivery-table-documents-content');
-        // var row = showTable.insertRow(0);
-        // var cell1 = row.insertCell(0);
-        // var cell2 = row.insertCell(1);
-        // cell1.innerHTML = "NEW CELL1";
-        // cell2.innerHTML = "NEW CELL2";
-
         // showTable.children('tbody').find('tr').detach();
-        var row = showTable.insertRow(0);
-        var itemCell = row.insertCell(0);
-        var quantityCell = row.insertCell(1);
-        var unitCell = row.insertCell(2);
+        // var row = showTable.insertRow(0);
+        // var itemCell = row.insertCell(0);
+        // var quantityCell = row.insertCell(1);
+        // var unitCell = row.insertCell(2);
 
-        itemCell.innerHTML = "BBQ Sauce";
-        quantityCell.innerHTML = "5000";
-        unitCell.innerHTML = "bot";
+        // itemCell.innerHTML = "BBQ Sauce";
+        // quantityCell.innerHTML = "5000";
+        // unitCell.innerHTML = "bot";
     });
+
+    $('.add-new-delivery-button').click(function() {
+        $(this).attr('disabled', true).addClass('button-disable');
+        $(this).parents().next().removeClass('d-none')
+    });
+
+    $('.done-add-new-delivery').click(function() {
+        setTimeout(() => {
+            $(this).parents('.content').find('.add-new-delivery-button').attr('disabled', false).removeClass('button-disable');
+            $(this).parents('.content').find('.add-new-delivery-button').parents().next().addClass('d-none');
+        }, 1500);
+    })
+    $('.cancel-add-new-delivery').click(function() {
+        $(this).parents('.content').find('.add-new-delivery-button').attr('disabled', false).removeClass('button-disable');
+        $(this).parents('.content').find('.add-new-delivery-button').parents().next().addClass('d-none');
+    })
 });
