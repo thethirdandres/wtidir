dcf43ea1e83d89b164c7cd00ec65930e08243eb8
Andres Tubelleja III <thethirdandres@gmail.com>
2021-03-09 11:37:33 +0800

    Done: Splash page

--- wtidir/landing/urls.py
+++ wtidir/landing/urls.py
@@ -6,7 +6,8 @@ from . import views
 app_name = 'landing'
 
 urlpatterns = [
-    # path('', views.login_view, name='login_view'),
-    path('', views.landing_view, name='landing_view'),
+    path('', views.splash_view, name='splash_view'),
+    path('login', views.login_view, name='login_view'),
+    path('landing', views.landing_view, name='landing_view'),
     
 ]
--- wtidir/landing/views.py
+++ wtidir/landing/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def splash_view(request):
+    return render(request, 'landing_templates/splash.html')
+
 def login_view(request):
     return render(request, 'landing_templates/login.html')
 
