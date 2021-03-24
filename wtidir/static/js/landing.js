49180926f3cc7c545a475336d3deeb79dc2aee3a
Mae Dagum <mayeghen@gmail.com>
2021-03-24 16:47:38 +0800

    1.1 UI Improvements in Splash and Login

--- wtidir/static/js/landing.js
+++ wtidir/static/js/landing.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
     $('.login-button').click(function() {
-        $(this).parents('.login-div').addClass('d-none');
+        alert("This is it");
         $(this).parents('.login-div').siblings('.main').removeClass('d-none');
+        $(this).parents('.login-div').addClass('d-none');        
         console.log("wahahhaa");
     });
 });
